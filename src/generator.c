e5ece7b4fac191c61c460ad2acb787f7baef4f5b
Jan Koval <jan.koval96@gmail.com>
2017-04-23 13:40:23 +0200

    creating fd

--- src/generator.c
+++ src/generator.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
 	
 int main(int argc, char* argv[]) {
-	char* name = argv[1];
+	char* filename = argv[1];
 	int n = atoi(argv[2]);
 	
-	printf("%s %d", name, n);
+	printf("%s %d", filename, n);
+    
+    int file_descriptor = open(filename, O_WRONLY | O_CREAT);
+    
+    write(file_descriptor, "test", 4);
+    
+    close(file_descriptor);
     
     return 0;
 }
