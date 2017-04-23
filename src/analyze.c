2c04a7f673df3e64c9f513a5558158e33173f947
Jan Koval <jan.koval96@gmail.com>
2017-04-23 16:14:50 +0200

    add analyze

---
+++ src/analyze.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	char* filename = argv[1];
+	int file_descriptor;
+	int res, i;
+	
+	file_descriptor = open(filename, O_RDONLY);
+	while(1) {
+		unsigned char number;
+		res = read(file_descriptor, &number, sizeof(unsigned char));
+		printf("%d", res);
+		printf("\n");
+		if(res <= 0) {
+			return 0;
+		} else {
+			float fields[number];
+			int tmp = read(file_descriptor, fields, sizeof(float) * number);
+			for(i = 0; i < number; i++) {
+				printf("%lf ", fields[i]);
+			}
+			printf("\n");	
+			printf("%d", tmp);	
+			printf("\n");
+		}
+	}
+	
+}
